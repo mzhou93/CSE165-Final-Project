87a6f7c38f0eeb8fcd25e57e3c401af940aca2eb
mzhou93 <michellezhou@yahoo.com>
2015-04-03 14:22:43 -0700

    Create app_main.cpp

---
+++ app_main.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+# include "app_window.h"
+
+
+//==========================================================================
+// Main routine
+//==========================================================================
+int main ( int argc, char** argv )
+ {
+   // Init freeglut library:
+   glutInit ( &argc,argv );
+   glutInitDisplayMode ( GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
+
+   // Now create the window of your application:
+   AppWindow* w = new AppWindow ( "Space Invaders", 300, 300, 640, 480 );
+
+   // Finally start the main loop:
+   w->run ();
+}

65efe87a751696084bc3845ac8e6698a14efc01c
lightningbending <jpg@itsdinc.com>
2015-04-03 16:20:31 -0700

    Create Enemy.h

---
+++ Enemy.h
@@ -0,0 +1,24 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+class Enemy{
+public:
+    float x, y, l, h;
+
+    Enemy(){}
+
+    Enemy(float x, float y, float l, float h) {
+        this->x = x;
+        this->y = y;
+        this->l = l;
+        this->h = h;
+    }
+
+    ~Enemy(){}
+
+    void draw();
+
+};
+
+
+#endif // ENEMY_H

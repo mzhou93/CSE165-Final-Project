6df0b53d930b6cae1dfd63aa123a2312e4c4dc5f
mzhou93 <michellezhou@yahoo.com>
2015-04-03 14:55:29 -0700

    Create app_window.cpp

---
+++ app_window.cpp
@@ -0,0 +1,62 @@
+
+# include <iostream>
+# include "app_window.h"
+
+AppWindow::AppWindow ( const char* label, int x, int y, int w, int h )
+          :GlutWindow ( label, x, y, w, h )
+ {
+   _markx = 0;
+   _marky = 0;
+   addMenuEntry ( "Option 0", evOption0 );
+   addMenuEntry ( "Option 1", evOption1 );
+	
+   user = new User(-0.05, -0.7, 0.1, 0.05);
+//   shoot = false;
+}
+
+// mouse events are in window coordinates, but your scene is in [0,1]x[0,1],
+// so make here the conversion when needed
+void AppWindow::windowToScene ( float& x, float &y )
+ {
+   x = (2.0f*(x/float(_w))) - 1.0f;
+   y = 1.0f - (2.0f*(y/float(_h)));
+ }
+
+// Called every time there is a window event
+void AppWindow::handle ( const Event& e )
+ {
+   bool rd=true;
+   user->handle(e);
+/*
+    if ( e.type == Keyboard ){
+		if (e.type == ' ')
+			shoot = true;
+	}
+
+	if (shoot)
+		user->draw();
+		*/
+
+   if (rd) redraw(); // ask the window to be rendered when possible
+}
+
+void AppWindow::resize ( int w, int h )
+ {
+   // Define that OpenGL should use the whole window for rendering
+   glViewport( 0, 0, w, h );
+   _w=w; _h=h;
+ }
+
+// here we will redraw the scene according to the current state of the application.
+void AppWindow::draw ()
+ {
+   glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+   glMatrixMode( GL_MODELVIEW );
+   glLoadIdentity();
+   
+   user->draw();
+
+   glFlush();         
+   glutSwapBuffers();
+}
+

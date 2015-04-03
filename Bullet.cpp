1f492c974eafd43e92e33555f51b8b9102795e5f
mzhou93 <michellezhou@yahoo.com>
2015-04-03 16:22:04 -0700

    Update Bullet.cpp

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,1 +1,15 @@
+#include "Bullet.h"
+#include "app_window.h"
 
+Bullet::Bullet(float x, float y){	//bullet constructor
+	this->x = x;
+	this->y = y;
+}
+
+void Bullet::draw(){	//draws bullet
+   glBegin(GL_LINE_STRIP);
+   glColor3d(0.0, 0.8, 0.8);
+   glVertex2f( x + 0.05, y + 0.035);
+   glVertex2f( x + 0.05, y + 0.07);
+   glEnd();
+}

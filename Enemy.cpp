2b5090e5914227663f34fa80212b11941a8647ee
lightningbending <jpg@itsdinc.com>
2015-04-03 16:20:09 -0700

    Create Enemy.cpp

---
+++ Enemy.cpp
@@ -0,0 +1,26 @@
+
+#include "Enemy.h"
+#include "app_window.h"
+
+#include <iostream>
+
+void Enemy::draw(){
+
+    glBegin(GL_POLYGON);
+    glColor3d(0.9, 0.0, 0.0);
+    glVertex2d(x, y);
+    glVertex2d(x + l, y);
+    glVertex2d(x + l, y - h);
+    glVertex2d(x, y - h);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+    glColor3d(0.9, 0.0, 0.0);
+    glVertex3f( x + 0.025, y+h-.05, 1.0);
+    glVertex3f( x + 0.05, y-0.1, -1.0);
+    glVertex3f( x + 0.075, y+h-.05, -1.0);
+
+    glEnd();
+
+}
+

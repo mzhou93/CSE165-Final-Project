cc262366e9c9055df09befc69bc1220b498c2053
mzhou93 <michellezhou@yahoo.com>
2015-04-03 16:22:23 -0700

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -1,1 +1,17 @@
+# ifndef BULLET_H
+# define BULLET_H
 
+# include "glut_window.h"
+
+class Bullet{
+public:
+	float x, y;
+
+	Bullet() {	}
+	Bullet(float x, float y);
+
+	~Bullet(){	}
+	void draw();
+};
+
+# endif

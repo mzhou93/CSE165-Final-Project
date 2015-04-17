2d5eb26d7321658fa8fab39f450654cbec2a43e0
lightningbending <jpg@itsdinc.com>
2015-04-16 17:39:21 -0700

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -1,23 +1,38 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
+#include <vector>
+
 class Enemy{
 public:
-    float x, y, l, h;
 
+    float x, y, l, h, r, g, b;
+
+    const int timersec = 5;
     Enemy(){}
 
-    Enemy(float x, float y, float l, float h) {
+    Enemy(float x, float y, float l, float h) { // default constructor will be red
         this->x = x;
         this->y = y;
         this->l = l;
         this->h = h;
     }
 
+    Enemy(float x, float y, float l, float h, float r, float g, float b) { // new constructor with changing the color
+        this->x = x;
+        this->y = y;
+        this->l = l;
+        this->h = h;
+        this->r = r;
+        this->g = g;
+        this->b = b;
+    }
+
     ~Enemy(){}
 
     void draw();
-	void handle();
+    void movement(int animate);
+
 };
 
 

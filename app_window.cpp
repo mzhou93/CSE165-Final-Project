9f1bd39e8ea66e2dc4407a61da8d57e8955e777d
lightningbending <jpg@itsdinc.com>
2015-05-08 14:28:36 -0700

    Update app_window.cpp

--- app_window.cpp
+++ app_window.cpp
@@ -1,4 +1,3 @@
-
 # include <iostream>
 # include "app_window.h"
 
@@ -7,19 +6,90 @@ AppWindow* AppWindow::window = NULL;
 AppWindow::AppWindow ( const char* label, int x, int y, int w, int h )
     :GlutWindow ( label, x, y, w, h )
 {
-	window = this;
+    window = this;
     _markx = 0;
     _marky = 0;
 
-    float increment = -0.75f; 
+    float increment = -0.75f;
 
     user = new User(-0.05f, -0.7f, 0.1f, 0.05f);
 
     for (int i = 0; i < 10; i++) { // create the aliens
-        army1.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+
+        if (i == 0) {
+            army1.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+            army1.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
+            army1.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
+            army1.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        }
+
+        if (i == 1) {
+            army2.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+            army2.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
+            army2.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
+            army2.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        }
+
+        if (i == 2) {
+            army3.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+            army3.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
+            army3.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
+            army3.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        }
+
+        if (i == 3) {
+            army4.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+            army4.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
+            army4.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
+            army4.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        }
+
+        if (i == 4) {
+            army5.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+            army5.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
+            army5.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
+            army5.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        }
+
+        if (i == 5) {
+            army6.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+            army6.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
+            army6.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
+            army6.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        }
+
+        if (i == 6) {
+            army7.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+            army7.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
+            army7.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
+            army7.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        }
+
+        if (i == 7) {
+            army8.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+            army8.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
+            army8.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
+            army8.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        }
+
+        if (i == 8) {
+            army9.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+            army9.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
+            army9.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
+            army9.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        }
+
+        if (i == 9) {
+            army10.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
+            army10.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
+            army10.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
+            army10.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        }
+
+        /*army1.push_back(new Enemy(increment, 0.85f, 0.1f, 0.05f, 0.0f, 0.0f, 0.7f));
         army2.push_back(new Enemy(increment, 0.72f, 0.1f, 0.05f, 0.7f, 0.0f, 0.0f));
         army3.push_back(new Enemy(increment, 0.59f, 0.1f, 0.05f, 0.0f, 0.7f, 0.0f));
-        army4.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));
+        army4.push_back(new Enemy(increment, 0.46f, 0.1f, 0.05f, 0.7f, 0.0f, 0.7f));*/
         increment += 0.15f;
     }
 
@@ -27,9 +97,9 @@ AppWindow::AppWindow ( const char* label, int x, int y, int w, int h )
 }
 
 AppWindow* AppWindow::instance(){
-	if(!window)
-		window = new AppWindow("Space Invaders", 300, 300, 640, 480);
-	return window;
+    if(!window)
+        window = new AppWindow("Space Invaders", 300, 300, 640, 480);
+    return window;
 }
 
 // mouse events are in window coordinates, but your scene is in [0,1]x[0,1],
@@ -45,15 +115,15 @@ void AppWindow::handle ( const Event& e )
 {
     bool rd=true;
     user->handle(e);
-	/*
-	for (int i = 0; i < 4; i++)
-		for (int j = 0; j < user->bullets.size(); j++){
-			cout << army1[i]->x << " " << army1[i]->y << endl;
-			if (army1[i]->y == user->bullets[j]->y){
-				user->bullets[i]->hit = true;
-			}
-		}
-		*/
+    /*
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < user->bullets.size(); j++){
+            cout << army1[i]->x << " " << army1[i]->y << endl;
+            if (army1[i]->y == user->bullets[j]->y){
+                user->bullets[i]->hit = true;
+            }
+        }
+        */
 
 
     if (rd) redraw(); // ask the window to be rendered when possible
@@ -76,14 +146,50 @@ void AppWindow::draw ()
 
     user->draw();
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < army1.size(); i++) {
         army1[i]->draw();
+    }
+
+    for (int i = 0; i < army2.size(); i++) {
         army2[i]->draw();
+    }
+
+    for (int i = 0; i < army3.size(); i++) {
         army3[i]->draw();
+    }
+
+    for (int i = 0; i < army1.size(); i++) {
+        army1[i]->draw();
+    }
+
+    for (int i = 0; i < army4.size(); i++) {
         army4[i]->draw();
+    }
+
+    for (int i = 0; i < army5.size(); i++) {
+        army5[i]->draw();
+    }
+
+    for (int i = 0; i < army6.size(); i++) {
+        army6[i]->draw();
+    }
+
+    for (int i = 0; i < army7.size(); i++) {
+        army7[i]->draw();
+    }
+
+    for (int i = 0; i < army8.size(); i++) {
+        army8[i]->draw();
+    }
+
+    for (int i = 0; i < army9.size(); i++) {
+        army9[i]->draw();
+    }
+
+    for (int i = 0; i < army10.size(); i++) {
+        army10[i]->draw();
     }
 
     glFlush();
     glutSwapBuffers();
 }
-

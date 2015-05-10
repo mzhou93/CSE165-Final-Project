0a3f313ca4c2902f195001923908165343a2fb8a
rollerboy247 <johnli1514850@gmail.com>
2015-05-10 12:58:25 -0700

    Create Text.cpp

---
+++ Text.cpp
@@ -0,0 +1,23 @@
+#include "app_window.h"
+#include "Text.h"
+#include "Enemy.h"
+
+void Text::drawText(const char* text, int length, float x, float y){
+	glMatrixMode(GL_PROJECTION);
+	double *matrix = new double[16];
+	glGetDoublev(GL_PROJECTION_MATRIX, matrix);
+	glLoadIdentity();
+	glOrtho(0, 800, 0, 600, -5, 5);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glLoadIdentity();
+	glRasterPos2i(x, y);
+	for(int i = 0; i < length; i++){
+		glutBitmapCharacter(GLUT_BITMAP_9_BY_15, (int)text[i]);
+	}
+	glPopMatrix();
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixd(matrix);
+	glMatrixMode(GL_MODELVIEW);
+}
